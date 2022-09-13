f4aa5c2106b0e7e9aafd4d78f7d1513698223b80
lamhoang1256 <lamhoang1256@gmail.com>
2022-09-13 12:04:38 +0700

    add folder server

---
+++ server/schemas/category.js
@@ -0,0 +1,22 @@
+export default {
+  name: "category",
+  title: "Category",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "url",
+      title: "Url",
+      type: "text",
+    },
+  ],
+};
---
+++ server/schemas/project.js
@@ -0,0 +1,76 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [{ type: "image" }],
+    },
+    {
+      name: "videoDemo",
+      title: "Video Demo",
+      type: "string",
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "content",
+      title: "content",
+      type: "markdown",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      });
+    },
+  },
+};
