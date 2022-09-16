ca8fb8a5f084d002ddbc0253e619ef50d11b0bf2
lamhoang1256 <lamhoang1256@gmail.com>
2022-09-16 22:04:06 +0700

    Chore: remove field in sanity.json

--- utils/sanityClient.ts
+++ utils/sanityClient.ts
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "mb7oy183", // you can find this in sanity.json
-  dataset: "production", // or the name you chose in step 1
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: "production",
   useCdn: true, // `false` if you want to ensure fresh data
 });
